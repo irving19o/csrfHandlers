a404749e99811a635c107f72039560bfc1666eae
Irving Luna <ilunadeveloper@gmail.com>
2019-06-27 01:58:34 -0500

    initial commit

---
+++ HelpersExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelpersExample.Global" Language="C#" %>
---
+++ HelpersExample/Handler.ashx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+
+namespace HelpersExample
+{
+    /// <summary>
+    /// Descripción breve de Handler
+    /// </summary>
+    public class Handler : IHttpHandler
+    {
+       
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            try
+            {
+                AntiForgery.Validate();
+                context.Response.Write("This request was validated");
+            }
+            catch{
+                context.Response.Write("");
+            }
+
+            
+
+           
+
+            /*if () {
+                context.Response.Write("This request was validated");
+            }
+            else {
+                context.Response.Write("This request was not validated");
+            }*/
+           
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
